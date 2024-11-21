2c305131aefab466198bfbe871fc80e5ba8f8cd7
ryan-lachance <r-lachance@hotmail.com>
2024-11-21 16:18:21 -0500

    Added support for more pages

--- scripts/monster.js
+++ scripts/monster.js
@@ -1,22 +1,36 @@
-const elements = document.querySelectorAll('.spell-tooltip');
-const jsonUrl = chrome.runtime.getURL('spells.json');
-console.log('Fetching from:', jsonUrl);  // Ensure this log appears
-
-fetch(jsonUrl)
-
-spells = {}
-spells['light'] = "2618996";
-
-elements.forEach(element => {
-    const link = element.href.split('/');
-    const spell_name = link[link.length - 1].split('-')[1];
-    console.log(spell_name)
 
 
-    if (element.textContent == 'light') {
-        console.log('tests')
-        new_spell_id = "2618996";
-        element.href = `https://www.dndbeyond.com/spells/${new_spell_id}-${spell_name}`;
-        element.setAttribute('data-tooltip-href', `//www.dndbeyond.com/spells/${new_spell_id}-tooltip?disable-webm=1&disable-webm=1`);
-    }
-});
+
+
+
+function populate() {
+    let elements = document.querySelectorAll('.spell-tooltip');
+    console.log("test")
+    elements.forEach(element => {
+        link = element.href.split('/');
+        spell_components = link[link.length - 1].split('-');
+        spell_components.shift();
+        spell_name = spell_components.join("-")
+        console.log(spell_name)
+
+
+
+        if (spells[spell_name]) {
+            console.log('tests')
+            new_spell_id = spells[spell_name]
+            element.href = `https://www.dndbeyond.com/spells/${new_spell_id}-${spell_name}`;
+            element.setAttribute('data-tooltip-href', `//www.dndbeyond.com/spells/${new_spell_id}-tooltip?disable-webm=1&disable-webm=1`);
+        }
+    });
+
+}
+
+
+spells = {
+    "light": "2618996",
+    "sacred-flame": "2618967"
+}
+
+
+
+setInterval(populate, 1000);
--- scripts/spells.js
+++
@@ -1,3 +0,0 @@
-export const spells = {
-    light: "2618996"
-};
